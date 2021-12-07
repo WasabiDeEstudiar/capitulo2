dec3681d98dba5f54c877ccec3267e788d280393
WasabiDeEstudiar <89497769+WasabiDeEstudiar@users.noreply.github.com>
2021-12-06 18:36:03 -0800

    Create dos.c

---
+++ dos.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+//@author   Luis Roberto Perez Garcia
+//@GitNick  WasabiDeEstudiar
+//@Date     06/12/2021
+
+void main (void)
+{
+int i , suma ;
+int vector [5]= {128 , 32 , 100 , -30 , 124};
+for ( suma = i = 0; i <5; i ++ )
+{
+suma += vector [i ];
+}
+printf (" La suma es %d \n" , suma );
+}
