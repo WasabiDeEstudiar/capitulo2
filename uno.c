3e50f9c86e72596e1a7a02c2c6933f2207991793
WasabiDeEstudiar <89497769+WasabiDeEstudiar@users.noreply.github.com>
2021-12-06 18:28:45 -0800

    Create uno.c

---
+++ uno.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//@author   Luis Roberto Perez Garcia
+//@Git-Nick WasabiDeEstudiar
+//@Date     06/12/2021
+
+int main()
+{
+int vi , vf , i ;
+for ( i= vi ; i <= vf ; i++ )
+{
+/*Se imprime la palabra hola*/
+printf ("Hola");
+}
+i= vi;
+while ( i <= vf )
+{
+/*Se imprime la palabra adios*/
+printf ("Adios");
+i++;
+}
+}
